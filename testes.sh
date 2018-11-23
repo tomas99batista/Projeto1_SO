f0a81ef16ca5fe33acd1e427aa5c4c1b450ecab1
tomas99batista <tomas99batista@gmail.com>
2018-11-23 04:50:33 +0000

    almost there

--- testes.sh
+++
@@ -1,9 +0,0 @@
-# files["$entry"]=$size
-# totalspace=$((size + totalspace))
-declare -A authors
-authors=( [Pushkin]=10050 [Gogol]=23 [Dostoyevsky]=9999 [Tolstoy]=23 )
-
-for k in "${!authors[@]}"
-do
-    echo $k ' - ' ${authors["$k"]}
-done | sort -rn -k3
